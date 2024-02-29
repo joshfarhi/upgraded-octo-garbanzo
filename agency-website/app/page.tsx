b8a67f013bb0b034b4c2bbd5b2831d35c53c2e41
Joshua <joshuafarhi@protonmail.com>
2024-02-28 23:19:59 -0800

    reset

--- agency-website/app/page.tsx
+++ agency-website/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import { Navbar } from './components/ui/navbar-menu';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Navbar />
+    <main className="">
+      
     </main>
   );
 }
