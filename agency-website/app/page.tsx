74bc8e4f1af91417a7ed0aa8917e22e0e3d958b4
Joshua <joshuafarhi@protonmail.com>
2024-02-29 00:59:37 -0800

    header

--- agency-website/app/page.tsx
+++ agency-website/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Hero from "@/app/components/hero"
+import Header from "@/app/components/header"
 
 export default function Home() {
   return (
     <main className="">
+      <Header />
       <Hero />
     </main>
   );
